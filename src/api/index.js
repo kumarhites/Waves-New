e42958bb66eb8cff3df6f067d0c17d4d8b0d28bc
Hitesh Kumar <hkhiteshkumar66@gmail.com>
2022-06-19 22:46:32 +0530

    Api key removed

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,7 @@ const options = {
   method: 'GET',
   
   headers: {
-    'X-RapidAPI-Key': 'f3eb4c1a78msh2d89f060ea21644p174ee3jsna5ac473991db',
+    'X-RapidAPI-Key': 'your-api-key',
     'X-RapidAPI-Host': 'ncsmusic.p.rapidapi.com'
   }
 };
